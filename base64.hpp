24a4c4e241265713af5ea42c332deb13c1dbe78e
BusyStudent <fyw90mc@gmail.com>
2020-03-20 11:48:15 +0800

    自己实现了base64的编解码 代替原来别人解码的代码

--- base64.hpp
+++ base64.hpp
@@ -1,15 +1,18 @@
 #ifndef _BOX_BASE64_HPP_
 #define _BOX_BASE64_HPP_
 #include <string>
+#include <cinttypes>
 namespace Box{
 	namespace Base64{
 		//Base64的表
-		extern const char *EncodeTable;
-		extern const char DecodeTable[];//解码表
-		std::string Decode(const char *str);
-		std::string Decode(const char *data,size_t datalen);
-		std::string Encode(const char *data,size_t datalen);
-		std::string Encode(const char *str);
+		extern const std::string Table;
+		std::string Decode(const std::string &str);
+		std::string Decode(const uint8_t *str);
+		std::string Decode(const uint8_t *data,size_t datalen);
+		//编码
+		std::string Encode(const std::string &str);
+		std::string Encode(const uint8_t *data,size_t datalen);
+		std::string Encode(const uint8_t *str);
 	};
 }
 #endif
