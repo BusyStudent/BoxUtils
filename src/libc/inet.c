ec78b58e72d053d5511a75d275ff160eb6efdc7d
BusyStudent <fyw90mc@gmail.com>
2020-07-10 13:26:38 +0800

    修复了在Window编译不过去的问题

--- src/libc/inet.c
+++ src/libc/inet.c
@@ -16,6 +16,7 @@
 #include "libc/inet.h"
 #include "libc.h"
 LIBC_BEGIN
+
 int Box_socket_init(){
     //Win32 初始化socket
     #ifdef _WIN32
@@ -72,5 +73,57 @@ int Box_bind(Box_socket_t sock,const void *addr,Box_socklen_t len){
 }
 Box_socket_t Box_accept(Box_socket_t sock,void *addr,Box_socklen_t *len){
     return accept(sock,(struct sockaddr*)addr,len);
+}
+//IP转地址
+int Box_inet_pton(int type,const char *str,void *addr){
+    #ifdef _WIN32
+    //win32
+    int len;
+    switch(type){
+        //类型
+        case AF_INET:
+            len = sizeof(struct sockaddr_in);
+            break;
+        case AF_INET6:
+            len = sizeof(struct sockaddr_in6);
+            break;
+        default:
+            //暂时不被支持
+            WSASetLastError(WSAEPROTONOSUPPORT);
+            return -1;
+    }
+    //这个函数的参数没有const不知道会不会改变参数
+    return WSAStringToAddressA(str,type,nullptr,addr,&len);
+
+    #else
+    return inet_pton(type,str,addr);
+    #endif
+}
+//地址转IP
+const char *Box_inet_ntop(int type,const void *addr,char *buf,size_t buflen){
+    #ifdef _WIN32
+    int len;
+    switch(type){
+        case AF_INET:
+            len = sizeof(struct sockaddr_in);
+            break;
+        case AF_INET6:
+            len = sizeof(struct sockaddr_in6);
+            break;
+        default:
+            WSASetLastError(WSAEPROTONOSUPPORT);
+            return -1;
+    }
+    //这个函数的参数没有const不知道会不会改变参数
+    if(WSAAddressToStringA(addr,len,nullptr,buf,buflen) != 0){
+        return nullptr;
+    }
+    else{
+        return buf;
+    }
+    
+    #else
+    return  inet_ntop(type,addr,buf,buflen);
+    #endif
 }
 LIBC_END
